a0d09882fe63c7e31c48befd0d2da8552e3502ac
Himanshu Dugar <122635852+dugarHimanshu@users.noreply.github.com>
2025-11-11 09:10:11 +0530

    Fix base path for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 
-
 const dev = process.argv.includes('dev');
 
 export default {
   kit: {
     adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: "index.html"   // ✅ required for dynamic routes
+      fallback: 'index.html'
     }),
     paths: {
-      base: dev ? '' : '/bhajanvani'   // ✅ required for GitHub Pages
+      base: dev ? '' : '/bhajanvani'
     },
     prerender: {
       crawl: true,
-      entries: ['*'] // ✅ generate all static routes
+      entries: ['*']
     }
   }
 };
